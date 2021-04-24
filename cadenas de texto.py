2aa9b23567be3ee621222492a97d35da540a1e89
Fabricio Franco <andre.fabricio2004@gmail.com>
2021-04-23 20:17:50 -0500

    Create cadenas de texto.py

---
+++ cadenas de texto.py
@@ -0,0 +1,11 @@
+#cadenas de texto
+a = 14
+b = 12 
+c = a + b
+print(a + b)
+print(a, "hola", b, '=', c)
+
+#print(a+ "hola"+ b+ '='+ c)
+
+#A la hora de concatenar número y cadenas de texto, es necesario convertir números a variables de texto 
+print(str(a) + "hola"+ str(b)+ '='+ str(c) )
