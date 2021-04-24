7a46748ec38e92de1c9db1f7e46ffd3bf5fb5e59
Fabricio Franco <andre.fabricio2004@gmail.com>
2021-04-23 19:47:47 -0500

    Create clase 03.py

---
+++ clase 03.py
@@ -0,0 +1,17 @@
+#La bienvenida XD
+print("Bienvenido a la Clase 03 de Mateapp")
+
+name = input("¿Cual es tu nombre?")
+
+print("Bienvenido" + name)
+
+
+#concatenación
+
+a = "uno"
+b = "dos"
+c = a + b 
+print(c)
+
+c = a * 3 
+print(c)
